984f37d6404e3ec98c13027ae6720f9e4c2d98e1
Sean Wentzel <sean@asana.com>
2023-10-22 19:19:48 -0700

    add loop detection

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,14 @@ function read(id: string): string {
     return (document.getElementById(id) as HTMLInputElement).value;
 }
 
+function renderResultStringHtml(result: Result) {
+    if (result.loop_of === null) {
+        return result.s;
+    } else {
+        const idx = result.s.indexOf(result.loop_of.s);
+        return `${result.s.substring(0,idx)}<b>${result.loop_of.s}</b>${result.s.substring(idx+result.loop_of.s.length)}`;
+    }
+}
 
 function handleInput() {
     const new_start = read("start_string");
@@ -30,7 +38,7 @@ function handleInput() {
         in_string = new_in_string;
         out_string = new_out_string;
         const results = generateResults(start, in_string, out_string);
-        const lis = results.map((result: Result) => `<li>${result.depth}: ${result.s} </li>`);
+        const lis = results.map((result: Result) => `<li>${result.depth}: ${renderResultStringHtml(result)} </li>`);
         const ul = `<ul>${lis.join("")}</ul>`
         const output = document.getElementById("output") as HTMLElement;
         output.innerHTML = ul;
@@ -41,19 +49,30 @@ type Result = {
     s: string;
     parent: Result | null;
     depth: number;
+    loop_of: Result | null;
+}
+
+function findLoop(at: Result, haystack: string): Result | null {
+    if (haystack.indexOf(at.s) !== -1) {
+        return at;
+    } else if (at.parent === null) {
+        return null;
+    } else {
+        return findLoop(at.parent, haystack);
+    }
 }
 
 function* children(result: Result, in_string: string, out_string: string) {
-    console.log("start children");
     let at = result.s.indexOf(in_string);
     while (at !== -1) {
         const replaced = result.s.substring(0, at) + out_string + result.s.substring(at+in_string.length);
+        const loop_of = findLoop(result, replaced);
         yield {
             s: replaced,
             parent: result,
-            depth: result.depth + 1
+            depth: result.depth + 1,
+            loop_of
         };
-        console.log(at);
         at = result.s.indexOf(in_string, at + 1);
       }
 }
@@ -62,14 +81,16 @@ function generateResults(start: string, in_string: string, out_string: string): 
     const firstResult: Result = {
         s: start,
         parent: null,
-        depth: 0
+        depth: 0,
+        loop_of: null
     }
     let results = [firstResult];
     let at = 0;
-    while(at < results.length && at < 100) {
-        for(let res of children(results[at], in_string, out_string)) {
-            console.log(res);
-            results.push(res);
+    while(at < results.length && at < 500) {
+        if(results[at].loop_of === null) {
+            for(let res of children(results[at], in_string, out_string)) {
+                results.push(res);
+            }
         }
         at += 1;
     }
